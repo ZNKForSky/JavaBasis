a16a2da3c8104520f275e23f628f8cf9e868c699
ZhangNingke <zhangningke@xftmleasing.com>
2020-12-20 00:02:20 +0800

    优化ThreadLocal案例代码及注释

--- src/thread/threadlocal/NoThreadLocal.java
+++ src/thread/threadlocal/NoThreadLocal.java
@@ -1,7 +1,10 @@
 package thread.threadlocal;
 
 /**
- * 类说明：
+ * @author Luffy
+ * @Classname NoThreadLocal
+ * @Description 本来想着实现线程分离，也就是说每个线程持有自己的变量，各个线程操作自己的变量，互不干扰，可运行结果一直在变。
+ * @Date 2020/12/17 13:37
  */
 public class NoThreadLocal {
     static Integer count = new Integer(1);
@@ -9,34 +12,32 @@ public class NoThreadLocal {
     /**
      * 运行3个线程
      */
-    public void StartThreadArray(){
-        Thread[] runs = new Thread[3];
-        for(int i=0;i<runs.length;i++){
-            runs[i]=new Thread(new TestTask(i));
-        }
-        for(int i=0;i<runs.length;i++){
-            runs[i].start();
+    public void StartThreadArray() {
+        for (int i = 0; i < 3; i++) {
+            new Thread(new TestTask(i)).start();
         }
     }
 
     /**
-     *类说明：
+     * @Description 完成任务id与count相加，并输出结果
      */
-    public static class TestTask implements Runnable{
+    public static class TestTask implements Runnable {
         int id;
-        public TestTask(int id){
+
+        public TestTask(int id) {
             this.id = id;
         }
+
         @Override
         public void run() {
-            System.out.println(Thread.currentThread().getName()+":start");
-            count = count+id;
-            System.out.println(Thread.currentThread().getName()+":"
-                    +count);
+            System.out.println(Thread.currentThread().getName() + ":start");
+            count = count + id;
+            System.out.println(Thread.currentThread().getName() + ":"
+                    + count);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         NoThreadLocal test = new NoThreadLocal();
         test.StartThreadArray();
     }
--- src/thread/threadlocal/UseThreadLocal.java
+++ src/thread/threadlocal/UseThreadLocal.java
@@ -1,60 +1,62 @@
 package thread.threadlocal;
 
 /**
- * 演示ThreadLocal的使用
- *
- * @author luffy
+ * @author Luffy
+ * @Classname UseThreadLocal
+ * @Description 演示使用 ThreadLocal实现线程间数据隔离。
+ * @Date 2020/12/17 13:52
  */
 public class UseThreadLocal {
-
     /**
-     * 创建ThreadLocal实例对象，并初始化
+     * 创建一个存放 Integer类型的 ThreadLocal实例并调用初始化方法设置线程本地变量的初始值为0，
+     * ThreadLocal实现了线程间数据隔离，所以是线程安全的。
      */
-    static ThreadLocal threadLocal = new ThreadLocal() {
+    static ThreadLocal threadLocalInter = new ThreadLocal<Integer>() {
+        /**
+         * 返回该线程局部变量的初始值，该方法是一个protected的方法，显然是为了让子类覆盖而设计的。
+         * 这个方法是一个延迟调用方法，在线程第1次调用get()或set(Object)时才执行，并且仅执行1次。
+         * ThreadLocal中的缺省实现直接返回一个null。
+         * @return 初始值
+         */
         @Override
-        protected Object initialValue() {
+        protected Integer initialValue() {
             return 1;
         }
     };
 
-    /**
-     * 运行3个线程
-     */
-    private void startThreads() {
+    public static void main(String[] args) throws InterruptedException {
+        /*开启三个线程*/
         for (int i = 0; i < 3; i++) {
-            try {
-                Thread thread = new Thread(new TestThread(i));
-                /*加延时把线程开启的时间错开*/
-                Thread.sleep(1000);
-                thread.start();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            new Thread(new WorkThread(i)).start();
+            /*加休眠错开线程开启时间*/
+            Thread.sleep(10);
         }
+        /**
+         * 将当前线程局部变量的值删除，目的是为了减少内存的占用，该方法是JDK 5.0新增的方法。需要指出的是，
+         * 当线程结束后，对应该线程的局部变量将自动被垃圾回收，所以显式调用该方法清除线程的局部变量并不是
+         * 必须的操作，但它可以加快内存回收的速度。
+         */
+        threadLocalInter.remove();
     }
 
     /**
      * 测试线程：线程的工作是将ThreadLocal变量的值变化，并写回，看看线程之间是否会互相影响
      */
-    public static class TestThread implements Runnable {
+    static class WorkThread implements Runnable {
         int id;
 
-        TestThread(int id) {
+        public WorkThread(int id) {
             this.id = id;
         }
 
         @Override
         public void run() {
             System.out.println(Thread.currentThread().getName() + ":start");
-            Integer i = (Integer) threadLocal.get();
-            id += i;
-            threadLocal.set(id);
-            System.out.println("threadLocal取出新值 = " + threadLocal.get());
+            /*该方法返回当前线程所对应的线程局部变量。*/
+            int oldValue = (int) threadLocalInter.get();
+            /*设置当前线程的线程局部变量的值。*/
+            threadLocalInter.set(id + oldValue);
+            System.out.println("线程" + Thread.currentThread().getName() + "中本地变量新值是： " + threadLocalInter.get());
         }
-    }
-
-    public static void main(String[] args) {
-        UseThreadLocal useThreadLocal = new UseThreadLocal();
-        useThreadLocal.startThreads();
     }
 }
